6701fb13d27b05a209dffb0a2a7585183a84a905
shoftee <shoftee@gmail.com>
2023-06-02 02:04:25 +0300

    enh: more styling

--- crates/ui/src/layout.rs
+++ crates/ui/src/layout.rs
@@ -37,23 +37,44 @@ pub fn Footer(cx: Scope) -> impl IntoView {
 pub fn Center(cx: Scope) -> impl IntoView {
     let _keyboard_events = use_keyboard_events(cx);
 
-    let state_signals = use_state_signals(cx);
-
-    let catnip = Signal::derive(cx, move || state_signals.resource.catnip.get());
-
     view! { cx,
         <main class="unscrollable">
             <div class="nav-container">
                 <div>"navigation goes here"</div>
                 <div class="main-container unscrollable">
-                    <div class="col unscrollable">
-                        <div>"Resources"</div>
-                        <div>"Catnip is: "<DecimalView value=catnip /></div>
+                    <div class="col resources-col unscrollable">
+                        <ResourcesContainer />
                     </div>
-                    <div class="col unscrollable"></div>
-                    <div class="col env-container unscrollable">"Calendar and History"</div>
+                    <div class="col controls-col unscrollable"></div>
+                    <div class="col environment-col unscrollable">
+                        <div class="env-container">"Calendar and History"</div>
+                    </div>
                 </div>
             </div>
         </main>
     }
+}
+
+#[component]
+pub fn ResourcesContainer(cx: Scope) -> impl IntoView {
+    let state_signals = use_state_signals(cx);
+
+    let catnip = Signal::derive(cx, move || state_signals.resource.catnip.get());
+
+    view! { cx,
+        <div class="resources-container">
+        <ul class="list-group resources-list">
+            <button class="list-group-item list-group-item-action expander">
+                <div>"Resources"</div>
+                <div><i class="bi bi-arrows-expand"></i></div>
+            </button>
+            <li class="list-group-item small">"catnip " <DecimalView value=catnip /></li>
+        </ul>
+        </div>
+    }
+}
+
+#[component]
+fn NoResources(cx: Scope) -> impl IntoView {
+    view! { cx, <li class="list-group-item">"Your paws are empty."</li> }
 }
