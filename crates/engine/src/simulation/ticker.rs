7b7bd76432166cb5a6e3be3d5f430d58c64241a0
shoftee <shoftee@gmail.com>
2025-01-07 16:07:57 +0200

    enh: abandon fixed crate, should've stuck to my guns...

--- crates/engine/src/simulation/ticker.rs
+++ crates/engine/src/simulation/ticker.rs
@@ -4,27 +4,25 @@ use bevy::{
     time::{Fixed, Time, TimePlugin},
 };
 
-type Fixed10 = fixed::FixedU64<fixed::types::extra::U10>;
-
 #[derive(Component, Debug)]
 pub struct Ticker {
-    scale: Fixed10,
+    scale: f64,
 
-    fractional: Fixed10,
+    fractional: f64,
     whole: u64,
     just_ticked: bool,
-    delta: Fixed10,
+    delta: f64,
 }
 
 #[derive(Resource)]
 pub struct TickRate {
-    pub seconds_per_tick: Fixed10,
+    pub seconds_per_tick: f64,
 }
 
 impl Default for TickRate {
     fn default() -> Self {
         Self {
-            seconds_per_tick: Fixed10::from_num(0.2),
+            seconds_per_tick: 0.2,
         }
     }
 }
@@ -34,22 +32,20 @@ impl Ticker {
         assert!(scale > 0, "rate should never be 0");
 
         Self {
-            scale: Fixed10::from_num(scale),
+            scale: scale as f64,
 
-            fractional: Fixed10::ZERO,
+            fractional: 0.0,
             whole: 0,
             just_ticked: false,
-            delta: Fixed10::ZERO,
+            delta: 0.0,
         }
     }
 
-    fn advance(&mut self, delta_ticks: Fixed10) {
-        let fixed_delta = delta_ticks / self.scale;
-        let new_fractional = self.fractional + fixed_delta;
-        self.delta = new_fractional - self.fractional;
-        self.fractional = new_fractional;
+    fn advance(&mut self, delta_ticks: f64) {
+        self.delta = round(delta_ticks / self.scale);
+        self.fractional = round(self.fractional + self.delta);
 
-        let new_whole = self.fractional.floor().to_num();
+        let new_whole = self.fractional.floor() as u64;
         self.just_ticked = self.whole != new_whole;
         self.whole = new_whole;
     }
@@ -57,6 +53,10 @@ impl Ticker {
     pub fn just_ticked(&self) -> bool {
         self.just_ticked
     }
+}
+
+fn round(number: f64) -> f64 {
+    (number * 1000.0).round() / 1000.0
 }
 
 pub mod schedule {
@@ -84,6 +84,6 @@ fn spawn(mut cmd: Commands) {
 
 fn advance_simulation(time: Res<Time>, tick_rate: Res<TickRate>, mut tickers: Query<&mut Ticker>) {
     for mut ticker in tickers.iter_mut() {
-        ticker.advance(Fixed10::from_num(time.delta().as_secs_f64()) / tick_rate.seconds_per_tick);
+        ticker.advance(time.delta().as_secs_f64() / tick_rate.seconds_per_tick);
     }
 }
