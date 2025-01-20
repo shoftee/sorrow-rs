96c2d6c04450642178eb30ad720f00e344c0c2ac
shoftee <shoftee@gmail.com>
2025-01-20 17:59:52 +0200

    fix: pawse/unpawse panic

--- crates/ui/src/components/mod.rs
+++ crates/ui/src/components/mod.rs
@@ -11,9 +11,10 @@ use crate::endpoint::use_endpoint;
 
 #[component]
 pub fn Button(#[prop(into)] intent: Signal<Intent>, children: ChildrenFn) -> impl IntoView {
+    let endpoint = use_endpoint();
     view! {
         <button
-            class="btn" type="button" on:click=move |_| use_endpoint().send(intent.get())
+            class="btn" type="button" on:click=move |_| endpoint.send(intent.get())
         >
             {children()}
         </button>
